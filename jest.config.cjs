a48f8e4baa5a54549803590b3a58683389a0b210
Ebuka peter <peterdbrainy5@gmail.com>
2025-12-08 22:52:58 +0100

    fix (unit test): fix testing config setup bug

---
+++ jest.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-jsdom',
+    transform: {
+        '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transformIgnorePatterns: ['/node_modules/'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+
+        '\\.(png|jpg|jpeg|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+        '^next/image$': '<rootDir>/__mocks__/next/image.tsx'
+    },
+};
